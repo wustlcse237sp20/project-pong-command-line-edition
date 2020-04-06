31cc05a526fd09874637cfe6f20ef6542573d00f
Joe Albert <jmalbert@email.wustl.edu>
2020-04-05 21:16:07 -0500

    cleaned up code & added java docs

--- pong/core/src/com/pongcle/game/Pong.java
+++ pong/core/src/com/pongcle/game/Pong.java
@@ -6,51 +6,36 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.pongcle.screens.SinglePlayer;
 import com.pongcle.screens.SinglePlayerVersusAI;
 import com.pongcle.screens.TwoPlayer;
-import java.io.*; //added by JA
-import java.util.*; //added by JA
+import java.io.*; 
+import java.util.*; 
 
 public class Pong extends Game {
 	public SpriteBatch batch;
 
+	/**
+	 * Creates new pong game mode based on command line arguments, currently three distinct game modes 
+	 */
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
 		Dictionary game_modes = new Hashtable();
-
 		game_modes.put("1", "Single Player");
 		game_modes.put("2", "Two Player");
 		game_modes.put("3", "Single Player VS AI");
 
-		//set defaults if none given
-		String playerMode = "1";
+		String gameMode = getGameMode();
 
-		//check for user input flag's to change game settings
-		try {
-			File config_file = new File("config.txt");
-			Scanner scanner = new Scanner(config_file);
-			while(scanner.hasNext()){
-				if(scanner.nextLine().equals("-m")) {
-					playerMode = scanner.nextLine();
-					System.out.println("Game mode changed to: " + game_modes.get(playerMode)); //may need some try / catch logic here if not found in dictionary
-				}
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("The Config File was not found.. please try again");
-		}
-
-		//set game mode as selected
-		if(playerMode.equals("1")){
+		if(gameMode.equals("1")){
 			this.setScreen(new SinglePlayer(this));
 		}
-		else if(playerMode.equals("2")){
+		else if(gameMode.equals("2")){
 			this.setScreen(new TwoPlayer(this));
 		}
-		else if(playerMode.equals("3")){
+		else if(gameMode.equals("3")){
 			this.setScreen(new SinglePlayerVersusAI(this));
 		}else {
-			System.out.println("given game mode is not valid => Usage: ./pong -m <game mode> where <game mode> is one of these options: (1=single player, 2=two player, 3=player vs AI)");
+			System.out.println("given player mode is not valid => Usage: ./pong -m <game mode> where <game mode> is one of these options: (1=single player, 2=two player, 3=player vs AI)");
 		}
 	}
 
@@ -63,5 +48,29 @@ public class Pong extends Game {
 	@Override
 	public void dispose () {
 		batch.dispose();
+	}
+	
+	/**
+	 * 
+	 * @return a String containing the selected game mode. 
+	 * If no game mode provided, single player will used as default. 
+	 */
+	public String getGameMode(){
+		String playerMode = "1";
+
+		try {
+			File config_file = new File("config.txt");
+			Scanner scanner = new Scanner(config_file);
+			while(scanner.hasNext()){
+				if(scanner.nextLine().equals("-m")) {
+					playerMode = scanner.nextLine();
+					System.out.println("Game mode changed to: " + game_modes.get(playerMode)); //may need some try / catch logic here if not found in dictionary
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("The Config File was not found.. defaulting to single player");
+		}
+		return playerMode;
 	}
 }
