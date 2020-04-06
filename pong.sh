2af4e097877b9c2e5359ac7dc443b91acecdac22
jmalbert97 <42746374+jmalbert97@users.noreply.github.com>
2020-04-05 21:29:56 -0500

    added documentation

--- pong.sh
+++ pong.sh
@@ -1,15 +1,21 @@
 #!/bin/bash 
 
+
+#README: this file is intended to create the config.txt file which contains the users's command line prefferences based on the flags they input and begins the pong game. 
+
+
 #check for old config file, if it exists delete so old settings are not re-used 
 if test -f pong/core/assets/config.txt ; then 
 	rm pong/core/assets/config.txt
 fi
+
 
 #loop through argument flags and add to config file, if any 
 for var in "$@"
 do 	
 	echo "$var" >> pong/core/assets/config.txt
 done
+
 
 #begin the pong game with user flag settings 
 cd pong
